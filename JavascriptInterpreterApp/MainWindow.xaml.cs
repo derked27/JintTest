a3c1b644c3f6bf152bfec844c7bf7c6c85c7fcab
Derek Rees <dbrees@verisk.com>
2020-06-10 13:05:58 -0600

    Initial commit

---
+++ JavascriptInterpreterApp/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Jint;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JavascriptInterpreterApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            textBox1.Text = "a + b - 1";
+            textBox.Text =
+@"{
+    ""a"": 5,
+    ""b"": 8,
+    ""c"": ""Hello "",
+    ""d"": {
+        ""e"": ""WORLD""
+    },
+    ""f"": [ 1, 2, 3 ],
+}";
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            var javascriptEngine = new Engine();
+            var tokens = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(textBox.Text);
+            List<int> ints = new List<int> { 1, 2, 3 };
+            errorTextBlock.Text = "";
+            textBlock.Text = "";
+
+            foreach (var key in tokens.Keys)
+            {
+                var value = tokens[key];
+                javascriptEngine.SetValue(key, value);
+            }
+
+            javascriptEngine.SetValue("arrayTest", ints);
+
+            try
+            {
+                var result = javascriptEngine.Execute(textBox1.Text)
+                    .GetCompletionValue();
+                textBlock.Text = result?.ToObject()?.ToString() ?? null;
+            }
+            catch (Exception exception)
+            {
+                errorTextBlock.Text = exception.Message;
+            }
+        }
+    }
+}
